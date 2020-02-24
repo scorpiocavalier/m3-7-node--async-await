d20d32e9c42bf0a0a5d7724c64736f4dd7dfeab4
Scott Morin <scottanthonymorin@gmail.com>
2020-02-24 13:14:50 -0500

    remove my API key!

--- __solution/exercise-2.js
+++ __solution/exercise-2.js
@@ -9,10 +9,30 @@
 // 3. Once you have it working, pass it a few address to see what the responses look like.
 // 4. Make sure to only return an object with lat/lng and not the whole response
 
+const opencage = require('opencage-api-client');
+
+
 function getAddressPosition(address) {
     const requestObj = {
         key: '<MY_API_KEY>',
         q: address
     };
 
+    return opencage.geocode(requestObj)
+        .then(data => {
+            if (data.status.code == 200) {
+                if (data.results.length > 0) {
+                    const place = data.results[0];
+                    return place;
+                }
+            } else {
+                // other possible response codes:
+                // https://opencagedata.com/api#codes
+                console.log('error', data.status.message);
+            }
+        })
+        .then(data => console.log(data))
+        .catch(error => console.log('error', error.message));
 }
+
+getAddressPosition('1455 Boulevard de Maisonneuve O, Montréal, QC H3G 1M8');
