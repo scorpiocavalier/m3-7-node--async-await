ab49dca64ff6550104e148b335b5e1786b547629
Arturo Chen <art.can.fly@gmail.com>
2020-07-12 21:02:09 -0400

    E0 done

--- __workshop/exercise-0.js
+++ __workshop/exercise-0.js
@@ -5,20 +5,20 @@
 const compareToTen = (num) => {
   return new Promise((resolve, reject) => {
     num > 10
-      ? resolve(num + ' is greater than 10, success!')
-      : reject(num + ' is less than 10, error!');
-  });
-};
+      ? resolve(`${num} is greater than 10, success!`)
+      : reject(`${num} is less than 10, error!`)
+  })
+}
 
 const handleCompareToTen = async (num) => {
   try {
-    const result = await compareToTen(num);
-    console.log(result);
-  } catch {
-    console.log(err);
+    const result = await compareToTen(num)
+    console.log(result)
+  } catch(err) {
+    console.log(err)
   }
-};
+}
 
 // Calling the function (that uses the Promise)
-handleCompareToTen(15);
-handleCompareToTen(8);
+handleCompareToTen(15)
+handleCompareToTen(8)
