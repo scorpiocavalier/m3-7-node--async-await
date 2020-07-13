51c099bcb98adc28782829b014ad10c4e0cf0a46
Arturo Chen <art.can.fly@gmail.com>
2020-07-13 14:26:58 -0400

    E1 done

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -5,21 +5,27 @@ const makeAllCaps = (array) => {
   return new Promise((resolve, reject) => {
     array.every((word) => typeof word === 'string')
       ? resolve(array.map((word) => word.toUpperCase()))
-      : reject('Error: Not all items in the array are strings!');
-  });
-};
+      : reject('Error: Not all items in the array are strings!')
+  })
+}
 
 const sortWords = (array) => {
   return new Promise((resolve, reject) => {
     array.every((word) => typeof word === 'string')
       ? resolve(array.sort())
-      : reject('Error: Something went wrong with sorting words.');
-  });
-};
+      : reject('Error: Something went wrong with sorting words.')
+  })
+}
 
 const textTransform = async (array) => {
-  // here
-};
+  try {
+    let allCaps = await makeAllCaps(array)
+    let sortedAllCaps = await sortWords(allCaps)
+    console.log(sortedAllCaps)
+  } catch (error) {
+    console.log(error)
+  }
+}
 
-textTransform(['cucumber', 'tomatos', 'avocado']);
-textTransform(['cucumber', 44, true]);
+textTransform(['cucumber', 'tomatos', 'avocado'])
+textTransform(['cucumber', 44, true])
